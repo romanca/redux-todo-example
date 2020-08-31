6dd36ff3254dbe9b91e85564c0022d5e83f68069
Jaroslav Oswald <jaroslav.oswald@globallogic.com>
2020-08-31 09:59:38 +0200

    - build folder

---
+++ build/precache-manifest.1505a8ba083b928baf41e83aa73085b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a732b0d4a3da0d5fe558d5de7ecff5",
+    "url": "/redux-todo-example/index.html"
+  },
+  {
+    "revision": "8b206430d7081527b901",
+    "url": "/redux-todo-example/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d3988bf4c0e77f23517b",
+    "url": "/redux-todo-example/static/js/2.62919222.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/redux-todo-example/static/js/2.62919222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b206430d7081527b901",
+    "url": "/redux-todo-example/static/js/main.91aedbaa.chunk.js"
+  },
+  {
+    "revision": "052df712c96a788e0699",
+    "url": "/redux-todo-example/static/js/runtime-main.57564606.js"
+  }
+]);
